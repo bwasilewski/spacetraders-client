dc49c90572f3087a66f51fe4e708702a980b84a1
Benjamin Wasilewski <benwasilewski@gmail.com>
2023-09-10 11:19:53 -0500

    fetch marketplace data

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+
 function Marketplace() {
+  const [market, setMarket] = useState(null)
+  const currentWaypoint = useSelector((state) => {
+    return state.app.waypoint
+  })
+
+  useEffect(() => {
+    if ( currentWaypoint !== null && market === null ) {
+      fetch(`http://localhost:3000/api/system/${currentWaypoint.systemSymbol}/waypoint/${currentWaypoint.symbol}/market`, {
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if ( data.error ) {
+            console.log(data.error)
+          } else {
+            console.log('Market: ', data.data)
+            setMarket(data.data)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }, [currentWaypoint, market])
   return (
     <>
       <section>
         <h1>Marketplace</h1>
+        { market && (
+          <>
+            {/* {console.log(market)} */}
+          </>
+        )}
       </section>
     </>
   )
